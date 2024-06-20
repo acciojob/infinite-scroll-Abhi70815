196fe24a80debb51d975bf9619e907c7fce02b10
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-20 22:19:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
-
+let ol= document.getElementById('infi-list');
+for(let i=1;i<=10;i++){
+let li=document.createElement('li');
+ol.appendChild(li);
+	if(i==10){
+		for (let j = 0; j < 2; j++) {
+let li=document.createElement('li');
+ol.appendChild(li);
+			
+li.innerText=`Item${i++}`;
+		}
+		
+	}
+	else{
+		
+	li.innerText=`Item${i}`;
+	}
+}
